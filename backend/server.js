5a3c74fc421787405677a7ecffc6248f1ff64746
Tushar09v <140047961+Tushar09v@users.noreply.github.com>
2026-04-21 13:42:39 +0530

    initial commit2

--- backend/server.js
+++ backend/server.js
@@ -17,18 +17,13 @@ const allowedOrigins = [
   "http://webhosting.s3-website.ap-south-1.amazonaws.com/" // Update placeholder with your actual production domain
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
+
+app.use(cors({
+  origin: "http://webhosting.s3-website.ap-south-1.amazonaws.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+app.options("*", cors());
 
 app.use(express.json());
 
